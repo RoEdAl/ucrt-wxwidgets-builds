9de4d774791dd91db5ab7897782dc49b32083f2b
RoEdAl <roed@onet.eu>
2022-08-26 14:50:30 +0200

    configuration: wxUSE_XRC=false

--- make-workdir.cmake
+++ make-workdir.cmake
@@ -58,6 +58,7 @@ IF(NOT EXISTS ${WX_TEST_FILE})
 		0004-CMake-define-_CRT_NONSTDC_NO_WARNINGS-for-MSVC-only.patch
 		0005-CMake-CPack.patch
 		0006-CMake-install-PDB-files.patch
+		0007-CMake-do-dot-override-wxUSE_XML-by-wxUSE_XLR.patch
 	)
 	FOREACH(P IN LISTS PATCH_SET)
 		ApplyPatch(${WX_WORKDIR}/patch/${P} ${WX_SRC_DIR})
